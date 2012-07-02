33c1da95cca12fdd9d48a5745449c40dbd7a047c
Phil Pratt-Szeliga <pcpratts@syr.edu>
2012-07-02 12:18:19 -0400

    adding ability to draw strings on bitmaps

--- bitmap_image.hpp
+++ bitmap_image.hpp
@@ -28,7 +28,8 @@
 #include <limits>
 #include <cmath>
 #include <cstdlib>
-
+#include <vector>
+#include <map>
 
 struct bitmap_file_header
 {
@@ -1634,6 +1635,52 @@ inline void hierarchical_psnr(bitmap_image& image1,bitmap_image& image2, const d
       hierarchical_psnr_r(0,0, image1.width(), image1.height(),image1,image2,threshold,colormap);
    }
 }
+
+class bitmap_font {
+public:
+
+  inline bitmap_font(bitmap_image& bmp)
+    : m_bmp(bmp)
+  {
+  }
+
+  inline void setColor(unsigned char r, unsigned char g, unsigned char b){
+    m_r = r;
+    m_g = g;
+    m_b = b;
+  }
+
+  inline void drawString(int x, int y, std::string str){
+    for(size_t i = 0; i < str.size(); ++i){
+      int index = m_charToIndex[str[i]];
+      std::vector<int> x_points = m_x[index];
+      std::vector<int> y_points = m_y[index];
+
+      for(size_t j = 0; j < x_points.size(); ++j){
+        int curr_x = x_points[j] + x;
+        int curr_y = y_points[j] + y;
+        if(curr_x < 0 || curr_y < 0 || curr_x >= m_bmp.width() || curr_y >= m_bmp.height()){
+          continue;
+        }
+        m_bmp.red_channel(curr_x, curr_y, m_r);
+        m_bmp.green_channel(curr_x, curr_y, m_g);
+        m_bmp.blue_channel(curr_x, curr_y, m_b);
+      }
+      x += m_width;
+    }
+  }
+
+protected:
+  unsigned char m_r;
+  unsigned char m_g;
+  unsigned char m_b;
+  bitmap_image& m_bmp;
+
+  std::map<char, int> m_charToIndex;
+  std::vector<std::vector<int> > m_x;
+  std::vector<std::vector<int> > m_y;
+  int m_width;    
+};
 
 class image_drawer
 {
